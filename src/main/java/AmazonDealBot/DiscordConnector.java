ca708c6f199231b4a399015c9110961c9ac2907a
noahe <noahe@noek.fritz.box>
2025-02-21 15:06:10 +0100

    2. Commit: hat noch gefehlt

--- src/main/java/AmazonDealBot/DiscordConnector.java
+++ src/main/java/AmazonDealBot/DiscordConnector.java
@@ -1,5 +1,6 @@
 package AmazonDealBot;
 
+//Hierüber die Daten an den Discord Server / die Discord API senden
 public class DiscordConnector {
 
 }
--- src/main/java/AmazonDealBot/KeepaProductFinderRequest.java
+++ src/main/java/AmazonDealBot/KeepaProductFinderRequest.java
@@ -1,6 +1,7 @@
 package AmazonDealBot;
 
-//
+//Hier die gesamte Keepa-API ProductFinderRequest ablegen, mit Tokenaufteilung auf Länder, sinnvolle Parameter
+//überlegen, die man von der Launcher Klasse übergeben bekommt (bspw Token Limit, Zeiten, etc.)
 public class KeepaProductFinderRequest {
 
 }
